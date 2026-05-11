cc8297e1d821508f9aeceb48f98b83c024730e61
Altior1 <thomas.rud@outlook.fr>
2026-05-11 10:04:05 +0200

    mix phx.gen.auth Accounts User users

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :soiree_plateau, :scopes,
+  user: [
+    default: true,
+    module: SoireePlateau.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: SoireePlateau.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :soiree_plateau,
   ecto_repos: [SoireePlateau.Repo],
   generators: [timestamp_type: :utc_datetime]
